191cbc786b2094290171954635f475735dd12b09
Bazilansari <154603848+Bazilansari@users.noreply.github.com>
2024-03-02 15:57:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+
+// VARIABLES FOR Strings
+
+// Q1
+var username;
+
+
+// Q2
+
+var myName;
+myName = 'Bazilansari'
+
+
+// Q3
+
+var message;
+message = 'Hello World'
+alert(message)
+
+
+// Q4
+
+var myName = 'Bazil Ansari'
+alert(myName)
+
+var myAge = '22 years old'
+alert(myAge)
+
+var myQualification = 'Intermediate'
+alert(myQualification)
+
+
+// Q5
+
+var Name = 'PIZZA \nPIZZ \nPIZ \nPI \nP'
+alert(Name)
+
+
+// Q6
+
+var email;
+email='My email address : bazilansari555@gmail.com'
+alert(email)
+
+
+// Q7
+
+var book;
+book = '“A smarter way to learn JavaScript”'
+alert(book)
+
+
+// Q8
+
+var script = 'Yah! I Can Write HTML content through JavaScript'
+document.write(script)
+
+
+// Q9
+
+var symbols = '“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”'
+alert(symbols)
+document.write(symbols)
+
+
+
+// VARIABLES FOR NUMBERS
+
+// Q1
+
+var Age;
+age = 'I am 22 Years Old'
+alert(age)
+
+
+// Q2
+
+var visitor;
+visitor = '“You have visited this site 14 times”.'
+document.write(visitor)
+
+
+// Q3
+
+var birthYear;
+birthYear = 'My birth year is 2002'
+document.write(birthYear)
+
+
+// Q4
+
+var visitorName = '“John Doe ordered 5 T-shirt(s) on XYZ Clothing store”'
+document.write(visitorName)
